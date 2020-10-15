950ad2319f3017639e14991a9ea53def5374dcf0
Ifeanyi Fredrick Okwuobi <fredrickokwuobi@gmail.com>
2020-10-15 19:06:24 +0100

    check test3

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,19 +1,14 @@
 import { Injectable } from '@angular/core';
-import { Http, Response } from '@angular/http';
-
-import 'rxjs/add/operator/map'
-import{Album} from './album';
-import{Observable} from 'rxjs/Observable';
+import {Http,Response} from "@angular/http";
+import {map} 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
+	private _albumUrl:'../assets/album.json'
+  constructor(private _http:Http) { }
 
-  private _albumUrl = '../assets/album.json';
-
-  constructor(private _http: Http) {}
-
-  getAlbum(id:number) : Observable<Album>{
-    return this._http.get(this._albumUrl).map(response =>
-      <Album>response.json()
-    );
+  getAlbum(id:number){
+  	return this._http.get(_albumUrl).map(response=>{
+  	response.json();
+  	})
   }
 }
